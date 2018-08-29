7770f9c11e04f32300c1f230acb1d375fd5a5872
Andrew Smith <andrew.smith@divido.com>
2018-08-29 16:55:51 +0100

    Fixed typo in Merchant config

--- Subscriber/TemplateRegistration.php
+++ Subscriber/TemplateRegistration.php
@@ -43,7 +43,7 @@ class TemplateRegistration implements SubscriberInterface
         $apiKey = $config["Api Key"];
         $key = preg_split("/\./",$apiKey);
 
-        $min_product_amount = (isset($config['Minimum Product Amount'])) ? $config['Minimum Product Amount'] : 0;
+        $min_product_amount = (isset($config['Minimum Amount'])) ? $config['Minimum Amount'] : 0;
         $args->getSubject()->View()->assign('min_product_amount',$min_product_amount);
 
         if($config['Small Price Widget']){
