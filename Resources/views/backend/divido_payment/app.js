191187c3ddc2c946fb618e94da4e88afa09a068f
Andrew Smith <andrew.smith@divido.com>
2018-09-06 14:04:10 +0100

    Removing all superflous branches remotely

--- Resources/views/backend/divido_payment/app.js
+++
@@ -1,5 +0,0 @@
-//{block name="backend/customer/application"}
-//      {$smarty.block.parent}
-//      {include file="backend/divido_payment/controller/local_controller.js"}
-//      {include file="backend/divido_payment/view/detail/plans_container.js"}
-//{/block}
