9608d2dbd144c7c933960eb6719537afc334928f
Andrew Smith <andrew.smith@divido.com>
2018-10-18 16:22:58 +0100

    Added PhpUnit

---
+++ Tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DividoPayment\Tests;
+
+use Shopware\Components\Test\Plugin\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(1);
+    }
+}
